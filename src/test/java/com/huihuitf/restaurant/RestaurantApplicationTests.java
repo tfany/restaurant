8afd66e75ecbe648c08086f32b65309361e0b80c
tfany <424047049@qq.com>
2019-12-25 09:41:22 +0800

    搭建spring boot环境

---
+++ src/test/java/com/huihuitf/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huihuitf.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
