33388ddbd3eb21f1f7849ab66f2d1b0195bf3e13
胡佳乐 <798190817@qq.com>
2020-01-03 11:33:56 +0800

    QR code and add administrator modification

--- restaurant-api/src/main/java/com/codeorder/controller/QRCodeController.java
+++ restaurant-api/src/main/java/com/codeorder/controller/QRCodeController.java
@@ -1,9 +1,12 @@
 
 package com.codeorder.controller;
 
+import com.codeorder.pojo.Shop;
+import com.codeorder.service.ShopService;
 import com.codeorder.utils.CommonResult;
 import com.codeorder.utils.QRCodeUtil;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,8 @@ import java.net.URL;
 @RestController
 @RequestMapping("manager")
 public class QRCodeController {
+    @Autowired
+    private ShopService shopService;
 
     /**
      *
@@ -23,15 +28,19 @@ public class QRCodeController {
      */
     @PostMapping("/getQRcode")
     public CommonResult<URL> createQRCode(@Param("tableNum") int tableNum) {
-        boolean flag=QRCodeUtil.createQRCode(tableNum,"http://www.baidu.com");
-        if(flag) {
-            try {
-                URL url=new URL("http://www.baidu.com");
-                return CommonResult.success(url);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
+        Shop shop=shopService.shopInfo();
+        if(shop.getUrl()!=null){
+            boolean flag=QRCodeUtil.createQRCode(tableNum,shop.getUrl());
+            if(flag) {
+                try {
+                    URL url=new URL(shop.getUrl());
+                    return CommonResult.success(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             }
+            return CommonResult.failed("二维码生成失败");
         }
-        return CommonResult.failed("二维码生成失败");
+        return CommonResult.failed("请输入店铺的URL");
     }
 }
