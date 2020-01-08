a052df6ab04dcc0bbd265cebc1dbaf6fe4aaa4a9
宋 <1346690324@qq.com>
2020-01-08 13:57:07 +0800

    implement function getOrderDetailByIdAndStatus,return the dishes finished

--- restaurant-api/src/main/java/com/codeorder/controller/OrderListController.java
+++ restaurant-api/src/main/java/com/codeorder/controller/OrderListController.java
@@ -1,6 +1,7 @@
 package com.codeorder.controller;
 
 import com.codeorder.pojo.Order;
+import com.codeorder.pojo.OrderList;
 import com.codeorder.service.OrderListService;
 import com.codeorder.service.OrderService;
 import com.codeorder.utils.CommonResult;
@@ -22,12 +23,21 @@ public class OrderListController {
 
     @Autowired
     private OrderListService orderListService;
+    @Autowired
+    private OrderService orderService;
 
     @GetMapping("/getOrderListDetail")
     public CommonResult<Object> getOrderListDetail(Integer orderId){
         return CommonResult.success(orderListService.getOrderListDetail(orderId));
     }
 
-
+    @GetMapping("/getOrderDetailByIdAndStatus")
+    public CommonResult<Object> getOrderDetailByIdAndStatus(String number){
+        Integer orderId = orderService.getOrderIdByNumber(number);
+        if(orderId==null){
+            return CommonResult.failed("未查到相关订单！");
+        }
+        return CommonResult.success(orderListService.queryOrderDetailByIdAndStatus(orderId,1));
+    }
 
 }
